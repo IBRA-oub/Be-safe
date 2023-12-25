490a20a3fa3597a0ece5adf6189ed6f5394e0dce
IBRA-oub <brahimbourih@gmail.com>
2023-12-25 20:32:29 +0100

    all models

---
+++ app/Models/AssurClient.php
@@ -0,0 +1,34 @@
+<?php
+
+class AssurClient{
+
+    private $userId;
+    private $Assurance_ID;
+    
+    public function __construct( $userId, $Assurance_ID){
+        $this->userId = $userId;
+        $this->Assurance_ID = $Assurance_ID;
+    }
+
+    public function getUserId(){
+        return $this->userId;
+    }
+
+    public function setUserId($userId){
+        $this->userId = $userId;
+    }
+
+    public function getAssurance_ID(){
+        return $this->Assurance_ID;
+    }
+
+    public function setAssurance_ID($Assurance_ID){
+        $this->Assurance_ID = $Assurance_ID;
+    }
+
+    
+}
+
+
+
+?>
